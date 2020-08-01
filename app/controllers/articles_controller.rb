d61c13703bcd8a9b49d8be94eafb58c0ffff696b
Paul Childs <pachilds@gmail.com>
2020-08-01 15:11:15 -0400

    Add ability to Delete articles.  Add Show and Edit links to Articles page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,21 @@ class ArticlesController < ApplicationController
 
   def show
 
+    # Show the article for the supplied ID
     @article = Article.find(params[:id])
 
   end
 
   def index
 
+    # List all the existing Articles
     @articles = Article.all
 
   end
 
   def new
 
+    # Setup a new Article
     # Need an instance created to avoid error when page loads (validaton check)
     @article = Article.new
 
@@ -21,12 +24,16 @@ class ArticlesController < ApplicationController
 
   def edit
 
+    # Setup to edit the article for the supplied ID
     # Need an instance created to avoid error when page loads (validaton check)
     @article = Article.find(params[:id])
 
   end
 
   def create
+
+    # Create the new article with the supplied inputs, then load the new Article page
+
     # Just shows the listed parameters on the screen
     # render plain: params[:article]
 
@@ -43,7 +50,7 @@ class ArticlesController < ApplicationController
 
     else
 
-      # Go back to new page if something goes wrong
+      # If something goes wrong, reload New page with validation messages
       render 'new'
 
     end
@@ -51,15 +58,27 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
+
+    # Update the article for the supplied ID, the reload the Article page
     @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article was successfully updated."
       redirect_to @article
     else
 
+      # If something goes wrong, reload Edit page with validation messages
       render 'edit'
+
     end
+
+  end
+
+  def destroy
+
+    # Destroy the article for the supplied ID, then load the Articles page
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  # resources :articles
-  # resources :articles, only: [:show, :index, :new, :create]
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
+  # Below are all the REST routes that can be exposed
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
