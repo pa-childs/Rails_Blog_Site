46c24d5e977a2aa702e9578f98e703decc1d2fe2
Paul Childs <pachilds@gmail.com>
2020-08-07 09:38:57 -0400

    Add Create User functionality and some related minor page changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,28 @@ class UsersController < ApplicationController
 
   end
 
+  def create
+
+    @user = User.new(user_params)
+    if @user.save
+
+      flash[:notice] = "#{@user.username} was successfully created."
+      redirect_to articles_path
+
+    else
+
+      render 'new'
+
+    end
+
+  end
+
+  private
+
+  def user_params
+
+    params.require(:user).permit(:username, :email, :password)
+
+  end
+
 end
