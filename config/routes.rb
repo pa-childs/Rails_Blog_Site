7dfb36812324b9ecdb51fe5680b6707ff00ab02c
Paul Childs <pachilds@gmail.com>
2020-08-07 08:34:43 -0400

    Add New and Edit User pages.  Modifications to Articles pages for changed variables. Minor style changes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :articles
   # Below are all the REST routes that can be exposed
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
