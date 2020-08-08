e5dae0a04329844bb3d73cec84899f6ea06d5d8d
Paul Childs <pachilds@gmail.com>
2020-08-08 16:16:42 -0400

    Add in ability to create session, make login functionality, navigation changes related to login, style changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  # Static pages
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  # Articles pages
   resources :articles
   # Below are all the REST routes that can be exposed
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+
+  # User pages
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  # Login page
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'sessions#destory'
+  delete 'logout', to: 'sessions#destroy'
 end
