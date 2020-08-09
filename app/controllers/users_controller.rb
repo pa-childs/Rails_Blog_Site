266d1064dcd9d292aceaad1c5b4fe32fc16ba079
Paul Childs <pachilds@gmail.com>
2020-08-09 08:58:46 -0400

    Add login helpers, modify navigation to use helpers, login user when signed up, and some clean-up of user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
 
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -21,13 +21,11 @@ class UsersController < ApplicationController
 
   def edit
 
-    @user = User.find(params[:id])
 
   end
 
   def update
 
-    @user = User.find(params[:id])
     if @user.update(user_params)
 
       flash[:notice] ="Your account information was successfully updated."
@@ -46,6 +44,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
 
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.username} was successfully created."
       redirect_to articles_path
 
@@ -58,6 +57,12 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+
+    @user = User.find(params[:id])
+
+  end
 
   def user_params
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def current_user
+
+    if session[:user_id]
+
+      # Get current user info from database unless it was already accessed
+      @current_user ||= User.find(session[:user_id])
+
+    end
+
+  end
+
+  def logged_in?
+
+    # logged in if current_user is true
+    !!current_user
+
+  end
+
 end
