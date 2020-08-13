77f86af62c311b3dfa610b72fdd5d3a08775684b
Paul Childs <pachilds@gmail.com>
2020-08-12 20:09:37 -0400

    Add Categories page with pagination and integration test.

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+
+  def setup
+
+    @category1 = Category.create(name: "Programming")
+    @category2 = Category.create(name: "Ruby on Rails")
+
+  end
+
+  test "should show categories listing" do
+
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category1), text: @category1.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+
+  end
+
+end
