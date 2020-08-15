ae0c810dc656403a464dac2f45d8a12a26ccc25c
Paul Childs <pachilds@gmail.com>
2020-08-15 08:06:33 -0400

    Add ability to edit Categories. Create partial and modify new page as a result.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,9 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_admin, except: [:index, :show]
 
   def show
 
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -17,6 +17,25 @@ class CategoriesController < ApplicationController
   def new
 
     @category = Category.new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @category.update(category_params)
+
+      flash[:notice] = "Category name updated successfully."
+      redirect_to @category
+
+    else
+
+      render 'edit'
+
+    end
 
   end
 
@@ -43,6 +62,12 @@ class CategoriesController < ApplicationController
   def category_params
 
     params.require(:category).permit(:name)
+
+  end
+
+  def set_category
+
+    @category = Category.find(params[:id])
 
   end
 
